b8f87d6048d0dc460b6a8e73f81d21b11a6e9af0
amanhex <80842658+amanhex@users.noreply.github.com>
2023-07-15 23:53:47 +0530

    Create pascalTriangle(Var2)(Brute).cpp
    Use the same nCr formula to calculate the whole row

---
+++ Arrays-I/Pascal's Triangle/Variation 2/Brute Approach/pascalTriangle(Var2)(Brute).cpp
@@ -0,0 +1,33 @@
+// Variation 2: Given the row number n. Print the n-th row of Pascal’s triangle.
+
+// Solution: Use the same approach as Var1 and find the whole row of Pascal's Triangle using nCr (num row == num cols --> for Pascal Triangle)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> pascalTriangleVar2Brute(int num){
+    vector<int> ans;
+    int res = 1;
+    for (int i = 0; i < num; i++){
+        for (int j = 0; j < i; j++){
+            res *= (num - j);
+            res /= (j + 1);
+        }
+        ans.push_back(res);
+        res = 1;
+    }
+    ans.push_back(1);
+    return ans;
+}
+
+int main(){
+    int num;
+    cin >> num;
+
+    vector<int> ans = pascalTriangleVar2Brute(num - 1);
+
+    for (auto i : ans)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
