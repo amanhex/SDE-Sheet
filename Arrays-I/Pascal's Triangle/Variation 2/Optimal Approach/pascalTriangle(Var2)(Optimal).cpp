553db0f805aadeab8332d1674747ad386a1f37b9
amanhex <80842658+amanhex@users.noreply.github.com>
2023-07-16 13:55:55 +0530

    Update pascalTriangle(Var2)(Optimal).cpp
    Obeserving the Pascal's Triangle Pattern to find some formula

--- Arrays-I/Pascal's Triangle/Variation 2/Optimal Approach/pascalTriangle(Var2)(Optimal).cpp
+++ Arrays-I/Pascal's Triangle/Variation 2/Optimal Approach/pascalTriangle(Var2)(Optimal).cpp
@@ -1,13 +1,18 @@
 // Variation 2: Given the row number n. Print the n-th row of Pascal’s triangle.
 
-// Solution:
+// Solution: observing the pattern to find some formulaa
 
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<int> pascalTriangleVar2Op(int num){
     vector<int> ans;
-
+    int res = 1;
+    ans.push_back(res);
+    for (int i = 1; i < num; i++){
+        res = res * (num - i) / i;
+        ans.push_back(res);
+    }
     return ans;
 }
 
@@ -15,7 +20,7 @@ int main(){
     int num;
     cin >> num;
 
-    vector<int> ans = pascalTriangleVar2Op(num - 1);
+    vector<int> ans = pascalTriangleVar2Op(num);
 
     for (auto i : ans)
         cout << i << " ";
