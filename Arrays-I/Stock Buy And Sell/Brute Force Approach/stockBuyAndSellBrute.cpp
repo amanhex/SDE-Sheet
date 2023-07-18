fde90f7679d2fa2282b0a385e6ece19b724cc98d
amanhex <80842658+amanhex@users.noreply.github.com>
2023-07-19 01:20:47 +0530

    Create stockBuyAndSellBrute.cpp

---
+++ Arrays-I/Stock Buy And Sell/Brute Force Approach/stockBuyAndSellBrute.cpp
@@ -0,0 +1,26 @@
+// Brute Force Approach: Iterate the array twice (check the profit with for each element) and return the maxProfit
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int stockBuyAndSellBrute(vector<int> arr){
+    int maxProfit = 0;
+    for (int i = 0; i < arr.size(); i++){
+        for (int j = i + 1; j < arr.size(); j++){
+            int profit = arr[j] - arr[i];
+            maxProfit = max(maxProfit, profit);
+        }
+    }
+    return maxProfit;
+}
+
+int main(){
+    int size;
+    cin >> size;
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    int ans = stockBuyAndSellBrute(arr);
+    cout << ans << endl;
+    return 0;
+}
