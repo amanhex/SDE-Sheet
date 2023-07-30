3346dff17849e256a8c43f8f62830b1a45c08925
amanhex <80842658+amanhex@users.noreply.github.com>
2023-07-30 16:47:38 +0530

    Create longestConsSeq(Optimal).cpp

---
+++ Arrays-IV/Longest Consecutive Sequence/Optimal Approach/longestConsSeq(Optimal).cpp
@@ -0,0 +1,34 @@
+// Optimal Approach: Using Sorting
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longConsSeqOp(vector<int>& nums){
+    if (nums.empty())
+        return 0;
+    sort(nums.begin(), nums.end());
+    int maxLength = 1, length = 1;;
+    for (int i = 1; i < nums.size(); i++){
+        if (nums[i] == nums[i - 1] + 1)
+            length++;
+        else if (nums[i] == nums[i - 1])
+            continue;
+        else{
+            maxLength = max(length, maxLength);
+            length = 1;
+        }
+    }
+    maxLength = max(length, maxLength);
+    return maxLength;
+}
+
+int main(){
+    int size;
+    cin >> size;
+    vector<int> nums(size);
+    for (int i = 0; i < size; i++)
+        cin >> nums[i];
+    int ans = longConsSeqOp(nums);
+    cout << ans << endl;
+    return 0;
+}
