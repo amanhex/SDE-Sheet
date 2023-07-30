f3c5fb656a0f7f7ca6369d9e2e1b5698abd05f56
amanhex <80842658+amanhex@users.noreply.github.com>
2023-07-30 16:46:58 +0530

    Create longestConsSeq(Better).cpp

---
+++ Arrays-IV/Longest Consecutive Sequence/Better Approach/longestConsSeq(Better).cpp
@@ -0,0 +1,36 @@
+// Better Approach: Using HashSet
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longConsSeqBetter(vector<int>& nums){
+    if (nums.empty())
+        return 0;
+    unordered_set<int> st;
+    int longest = 1;
+    for (auto i : nums)
+        st.insert(i);
+    for (auto it : st){
+        if (st.find(it - 1) == st.end()) {
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+}
+
+int main(){
+    int size;
+    cin >> size;
+    vector<int> nums(size);
+    for (int i = 0; i < size; i++)
+        cin >> nums[i];
+    int ans = longConsSeqBetter(nums);
+    cout << ans << endl;
+    return 0;
+}
