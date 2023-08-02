5dba08261c4e0ccfc0f7b121f8810c10a52e3605
amanhex <80842658+amanhex@users.noreply.github.com>
2023-08-02 17:09:20 +0530

    Update search(Better).cpp

--- Arrays-III/Search in a 2D Matrix/Better Approach/search(Better).cpp
+++ Arrays-III/Search in a 2D Matrix/Better Approach/search(Better).cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool searchBetter2(vector<vector<int>> matrix, int target){
     int n = matrix.size(), m = matrix[0].size();
     int left = 0, right = m - 1;
-    while (left <= right){
+    while (left < n && right >= 0){
         if (matrix[left][right] == target)
             return true;
         else if (matrix[left][right] > target)
