74b8d8c01f9f7ec9809286819db9f1e9bb084088
amanhex <80842658+amanhex@users.noreply.github.com>
2023-08-06 17:03:34 +0530

    Update reverseLL(Rec).cpp

--- Linked List-I/Reverse a Linked List/Recursive Approach/reverseLL(Rec).cpp
+++ Linked List-I/Reverse a Linked List/Recursive Approach/reverseLL(Rec).cpp
@@ -1,23 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if (head == nullptr || head -> next == nullptr)
-            return head;
-        ListNode* newHead = reverseList(head -> next);
-        head -> next -> next = head;
-        head -> next = nullptr;
-        return newHead;
-    }
-};
+#include <bits/stdc++.h>
+#include "../../Node.h"
+using namespace std;
 
-// T.C: O(n) and S.C: O(n) {Because of n recursive calls}
+Node* reverseRec(Node* head){
+    if (head == nullptr || head -> next == nullptr)
+        return head;
+    Node* newHead = reverseRec(head -> next);
+    head -> next -> next = head;
+    head -> next = nullptr;
+    return newHead;
+}
+
+int main(){
+    Node* head = nullptr;
+    head -> push_back(head, 1);
+    head -> push_back(head, 2);
+    head -> push_back(head, 3);
+    head -> push_back(head, 4);
+    head -> push_back(head, 5);
+
+    // Linked List Before Reversal
+    head -> display(head);
+
+    Node* newHead = reverseRec(head);
+
+    // Linked List After Reversal
+    head -> display(newHead);
+    return 0;
+}
