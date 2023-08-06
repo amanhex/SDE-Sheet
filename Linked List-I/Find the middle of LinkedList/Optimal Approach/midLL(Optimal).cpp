7c3b82945ebc839a33978ab5c7d9b4c5dd1761ed
amanhex <80842658+amanhex@users.noreply.github.com>
2023-08-06 17:16:57 +0530

    Update midLL(Optimal).cpp

--- Linked List-I/Find the middle of LinkedList/Optimal Approach/midLL(Optimal).cpp
+++ Linked List-I/Find the middle of LinkedList/Optimal Approach/midLL(Optimal).cpp
@@ -1,24 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != nullptr && fast -> next != nullptr){
-            slow = slow -> next;
-            fast = fast -> next -> next;
-        }
-        return slow;
-    }
-};
+#include <bits/stdc++.h>
+#include "../../Node.h"
+using namespace std;
 
-// T.C: O(n) & S.C: O(1)
+// T.C: O(n) && S.C: O(1)
+Node* midLLBrute(Node* head){
+    Node* slow = head, *fast = head;
+    while (fast && fast -> next){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    return slow;
+}
+
+int main(){
+    Node* head = nullptr;
+    head -> push_back(head, 1);
+    head -> push_back(head, 2);
+    head -> push_back(head, 3);
+    head -> push_back(head, 4);
+    head -> push_back(head, 5);
+
+    Node* mid = midLLBrute(head);
+    cout << "Mid Node: " << mid -> data << endl;
+    return 0;
+}
