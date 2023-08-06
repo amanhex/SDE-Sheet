3ce9c80c94f7ca8f76b5315524fe54590d5dc9dc
amanhex <80842658+amanhex@users.noreply.github.com>
2023-08-06 17:09:55 +0530

    Update midLL(Brute).cpp

--- Linked List-I/Find the middle of LinkedList/Brute Force Approach/midLL(Brute).cpp
+++ Linked List-I/Find the middle of LinkedList/Brute Force Approach/midLL(Brute).cpp
@@ -1,30 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int length = 0;
-        // Finding length of LinkedList
-        while (temp){
-            length++;
-            temp = temp -> next;
-        }
+#include <bits/stdc++.h>
+#include "../../Node.h"
+using namespace std;
 
-        // Finding mid of LinkedList by traversing half of the LinkedList
-        temp = head;
-        for (int i = 0; i < length / 2; i++)
-            temp = temp -> next;
-        return temp;
+Node* midLLBrute(Node* head){
+    int length = 0;
+    Node* temp = head;
+    while (temp){
+        length++;
+        temp = temp -> next;
     }
-};
+    temp = head;
+    for (int i = 0; i < length / 2; i++)
+        temp = temp -> next;
+    return temp;
+}
 
-// T.C: O(n) + O(n / 2) & S.C: O(1)
+int main(){
+    Node* head = nullptr;
+    head -> push_back(head, 1);
+    head -> push_back(head, 2);
+    head -> push_back(head, 3);
+    head -> push_back(head, 4);
+    head -> push_back(head, 5);
+
+    Node* mid = midLLBrute(head);
+    cout << "Mid Node: " << mid -> data << endl;
+    return 0;
+}
