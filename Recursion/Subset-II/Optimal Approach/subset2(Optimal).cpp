621019bb721980b7bbfecaeb3cc6cbc817da3b34
amanhex <80842658+amanhex@users.noreply.github.com>
2023-08-23 19:25:31 +0530

    Update subset2(Optimal).cpp

--- Recursion/Subset-II/Optimal Approach/subset2(Optimal).cpp
+++ Recursion/Subset-II/Optimal Approach/subset2(Optimal).cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void subset2Helper(vector<int>& nums, vector<int>& currSubset, set<vector<int>>& st, int index){
-  if (index == nums.size()){
-    st.insert(currSubset);
-    return;
+void subset2Helper(vector<int>& nums, vector<int>& currSubset, vector<vector<int>>& res, int index){
+  res.push_back(currSubset);
+  for (int i = index; i < nums.size(); i++){
+    if (i != index && nums[i] == nums[i - 1])
+      continue;
+    currSubset.push_back(nums[i]);
+    subset2Helper(nums, currSubset, res, i + 1);
+    currSubset.pop_back();
   }
-  currSubset.push_back(nums[index]);
-  subset2Helper(nums, currSubset, st, index + 1);
-  currSubset.pop_back();
-  subset2Helper(nums, currSubset, st, index + 1);
   return;
 }
 
 vector<vector<int>> subset2Op(vector<int>& nums){
   sort(nums.begin(), nums.end());
   vector<int> currSubset;
-  set<vector<int>> st;
-  subset2Helper(nums, currSubset, st, 0);
-  vector<vector<int>> res(st.begin(), st.end());
+  vector<vector<int>> res;
+  subset2Helper(nums, currSubset, res, 0);
   return res;
 }
 
