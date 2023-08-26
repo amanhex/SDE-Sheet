603f11f1ea44f8ffb52bebe0ccbd48e868e64a97
amanhex <80842658+amanhex@users.noreply.github.com>
2023-08-26 18:36:38 +0530

    Update subset2(Brute).cpp

--- Recursion/Subset-II/Brute Force Approach/subset2(Brute).cpp
+++ Recursion/Subset-II/Brute Force Approach/subset2(Brute).cpp
@@ -41,3 +41,20 @@ vector<vector<int>> subset2Brute2(vector<int>& nums){
   return res;
 }
 
+int main(){
+  int size;
+  cin >> size;
+  vector<int> nums(size);
+  for (int i = 0; i < size; i++)
+    cin >> nums[i];
+
+  vector<vector<int>> res = subset2Brute2(nums);
+
+  for (int i = 0; i < res.size(); i++){
+    for (int j = 0; j < res[i].size(); j++)
+      cout << res[i][j] << " ";
+    cout << endl;
+  }
+
+  return 0;
+}
